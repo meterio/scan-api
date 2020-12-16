d9186dbc63689bb4390edb48517a5f6fe5de0e13
simonzg <zhanghan.simon@gmail.com>
2020-12-16 15:42:56 +0800

    update accounts api with page/limit settings

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
-import { try$, HttpError } from 'express-toolbox';
+import { HttpError, try$ } from 'express-toolbox';
+
 import Controller from '../interfaces/controller.interface';
 import AccountRepo from '../repo/account.repo';
 import TransferRepo from '../repo/transfer.repo';
@@ -36,7 +37,18 @@ class AccountController implements Controller {
 
   private getTxsByAccount = async (req, res) => {
     const { address } = req.params;
-    const txs = await this.txRepo.findByAccount(address);
+    let page = 1,
+      limit = 10;
+    if (typeof req.query.page === 'string') {
+      page = Number(req.query.page);
+      page = page > 1 ? page : 1;
+    }
+    if (typeof req.query.limit === 'string') {
+      limit = Number(req.query.limit);
+      limit = limit > 0 ? limit : 10;
+    }
+    const txs = await this.txRepo.findByAccount(address, page, limit);
+
     if (!txs) {
       return res.json({ txs: [] });
     }
@@ -45,7 +57,22 @@ class AccountController implements Controller {
 
   private getTransfersByAccount = async (req, res) => {
     const { address } = req.params;
-    const transfers = await this.transferRepo.findByAccount(address);
+    let page = 0,
+      limit = 10;
+    if (typeof req.query.page === 'string') {
+      page = Number(req.query.page);
+      page = page > 1 ? page : 1;
+    }
+    if (typeof req.query.limit === 'string') {
+      limit = Number(req.query.limit);
+      limit = limit > 0 ? limit : 10;
+    }
+
+    const transfers = await this.transferRepo.findByAccount(
+      address,
+      page,
+      limit
+    );
     if (!transfers) {
       return res.json({ transfers: [] });
     }
