70d90328774ff5fd676ded016c2496ab44209667
simonzg <zhanghan.simon@gmail.com>
2020-12-16 15:43:27 +0800

    display actual values for staking facts in metric APIs

--- src/controllers/metric.controller.ts
+++ src/controllers/metric.controller.ts
@@ -1,7 +1,8 @@
+import BigNumber from 'bignumber.js';
 import { Router } from 'express';
 import { try$ } from 'express-toolbox';
 
-import { MetricName, enumVals } from '../const';
+import { MetricName, UNIT_WEI, enumVals } from '../const';
 import Controller from '../interfaces/controller.interface';
 import MetricRepo from '../repo/metric.repo';
 
@@ -15,10 +16,10 @@ class MetricController implements Controller {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}/all`, try$(this.getMetric));
+    this.router.get(`${this.path}/all`, try$(this.getAllMetric));
   }
 
-  private getMetric = async (req, res) => {
+  private getAllMetric = async (req, res) => {
     const names = enumVals(MetricName);
     const metrics = await this.metricRepo.findByKeys(names);
     let map: { [key: string]: string } = {};
@@ -26,12 +27,18 @@ class MetricController implements Controller {
       map[m.key] = m.value;
     }
 
+    const buckets = map[MetricName.BUCKETS];
+
+    let totalStaked = new BigNumber(0);
+    for (const b of JSON.parse(buckets)) {
+      totalStaked = totalStaked.plus(b.totalVotes);
+    }
+
     return res.json({
       mtrg: {
         price: map[MetricName.MTRG_PRICE],
         priceChange: map[MetricName.MTRG_PRICE_CHANGE],
-        avgDailyReward: '235000000000000000000', // Wei
-        totalStaked: '12345000000000000000000', // Wei
+        avgDailyReward: '235 MTRG', // FIXME: fake data
       },
       mtr: {
         price: map[MetricName.MTR_PRICE],
@@ -40,14 +47,20 @@ class MetricController implements Controller {
       pos: {
         best: Number(map[MetricName.POS_BEST]),
         kblock: Number(map[MetricName.KBLOCK]),
-        validators: 11,
-        onlineNodes: 11,
-        totalNodes: 11,
+      },
+      staking: {
+        buckets: Number(map[MetricName.BUCKET_COUNT]),
+        candidates: Number(map[MetricName.CANDIDATE_COUNT]),
+        validators: Number(map[MetricName.STAKEHOLDER_COUNT]),
+        delegates: Number(map[MetricName.DELEGATE_COUNT]),
+        onlineNodes: 0, // FIXME: fake data
+        totalNodes: Number(map[MetricName.DELEGATE_COUNT]),
+        totalStaked: `${totalStaked.dividedBy(UNIT_WEI).toFixed()} MTRG`,
       },
       pow: {
         best: Number(map[MetricName.POW_BEST]),
-        difficulty: Number(map[MetricName.DIFFICULTY]),
-        hashrate: Number(map[MetricName.HASHRATE]),
+        difficulty: map[MetricName.DIFFICULTY],
+        hashrate: map[MetricName.HASHRATE],
       },
     });
   };
