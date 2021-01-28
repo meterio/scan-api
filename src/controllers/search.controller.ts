6c193fe77509ded348155f304ca26fb89f0eb9c2
simonzg <zhanghan.simon@gmail.com>
2021-01-28 12:11:47 +0800

    support search by number

--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -27,18 +27,31 @@ class SearchController implements Controller {
   private searchByHash = async (req, res) => {
     const { hash } = req.params;
 
+    let number = -1;
+    try {
+      number = Number(hash);
+      const block = await this.blockRepo.findByNumber(number);
+      if (block) {
+        return res.json({ type: 'block', data: block });
+      }
+    } catch (e) {
+      console.log('could not find by number');
+    }
     const block = await this.blockRepo.findByHash(hash);
-    const tx = await this.txRepo.findByHash(hash);
-    const account = await this.accountRepo.findByAddress(hash);
     if (block) {
       return res.json({ type: 'block', data: block });
     }
+
+    const tx = await this.txRepo.findByHash(hash);
     if (tx) {
       return res.json({ type: 'tx', data: tx });
     }
+
+    const account = await this.accountRepo.findByAddress(hash);
     if (account) {
       return res.json({ type: 'address', data: account });
     }
+
     if (isAddress(hash)) {
       return res.json({ type: 'address', data: { address: hash } });
     }
