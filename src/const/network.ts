c14979af8cf37bc94572c300f9e94a0a58189c22
kda-data <kda-data@protonmail.com>
2021-12-28 13:50:03 +0800

    update mainNet genesis hash

--- src/const/network.ts
+++ src/const/network.ts
@@ -1,5 +1,5 @@
 export enum Network {
-  MainNet = '0x00000000733c970e6a7d68c7db54e3705eee865a97a07bf7e695c63b238f5e52', // mainnet
+  MainNet = '0x000000008d23443cdb7c8b00391d41d957eab0d5bf812a1657f73f75ead5f997', // mainnet
   TestNet = '0x00000000ed77a5a4cc2cb585ed7fba4200b89751142cd6fe124aecc3d3350e58', // warringstakes
   DevNet = '0x00000000ed77a5a4cc2cb585ed7fba4200b89751142cd6fe124aecc3d3350e58', // tetra
 }
