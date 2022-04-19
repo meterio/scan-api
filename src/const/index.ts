36f01fb92f98d05d23ce14d130a1b6b292c10945
Simon Zhang <zhanghan.simon@gmail.com>
2022-04-19 15:54:53 -0400

    minor fix

--- src/const/index.ts
+++ src/const/index.ts
@@ -13,13 +13,11 @@ export const LIMIT_WINDOW = 10;
 export const UNIT_SHANNON = 1e9;
 export const UNIT_WEI = 1e18;
 
-const { network, standby, consts, restfulEndpoint } = parseNetwork(
-  process.env.NETWORK
-);
+const { network, standby, consts } = parseNetwork(process.env.NETWORK);
 export const getEnvNetwork = () => parseNetwork(process.env.NETWORK);
 export const ENERGY_SYM = consts.energySym;
 export const BALANCE_SYM = consts.balanceSym;
 export const CHAIN_ID = consts.chainId;
 export const STANDBY = standby;
 export const NETWORK = network;
-export const RESTFUL_ENDPOINT = restfulEndpoint;
+export const RESTFUL_ENDPOINT = consts.restfulEndpoint;
