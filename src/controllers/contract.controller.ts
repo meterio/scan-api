da6928edf13e036e07ccccc33b22f83a3681e735
yanfeizuo <18856856693@163.com>
2022-06-28 15:54:35 +0800

    update scan-db;add api/contract/:address in contract.controller

--- src/controllers/contract.controller.ts
+++ src/controllers/contract.controller.ts
@@ -1,4 +1,4 @@
-import { ContractFileRepo } from '@meterio/scan-db/dist';
+import { ContractFileRepo, ContractRepo } from '@meterio/scan-db/dist';
 import { Request, Response, Router } from 'express';
 import { try$ } from 'express-toolbox';
 
@@ -6,8 +6,10 @@ import Controller from '../interfaces/controller.interface';
 
 class ContractController implements Controller {
   public path = '/api/cfile';
+  public path1 = '/api/contract';
   public router = Router();
   private contractFileRepo = new ContractFileRepo();
+  private contractRepo = new ContractRepo();
 
   constructor() {
     this.initializeRoutes();
@@ -15,6 +17,13 @@ class ContractController implements Controller {
 
   private initializeRoutes() {
     this.router.get(`${this.path}/:address`, try$(this.getContractFiles));
+    this.router.get(`${this.path1}/:address`, try$(this.getContractByAddress))
+  }
+
+  private getContractByAddress = async (req: Request, res: Response) => {
+    const { address } = req.params;
+    const contract = await this.contractRepo.findByAddress(address);
+    return res.json({ contract: contract.toJSON() })
   }
 
   private getContractFiles = async (req: Request, res: Response) => {
