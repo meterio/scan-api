a6d7b14ca959b257fde98fda86341d7b0accb71e
Simon Zhang <zhanghan.simon@gmail.com>
2022-08-09 10:44:57 -0400

    fine grind of nft api

--- src/controllers/nft.controller.ts
+++ src/controllers/nft.controller.ts
@@ -4,7 +4,7 @@ import { try$ } from 'express-toolbox';
 import { getCuratedNFTs, getEnvNetwork } from '../const';
 
 import Controller from '../interfaces/controller.interface';
-import { extractPageAndLimitQueryParam } from '../utils/utils';
+import { blockIDtoNum, extractPageAndLimitQueryParam } from '../utils/utils';
 
 class NFTController implements Controller {
   public path = '/api/nfts';
@@ -40,10 +40,20 @@ request: page, limit
 return: collection list [nftAddress, nftCreator, createTxHash, createBlockNumber, nftName, nftSymbol, nftType]
 
     */
-    return res.json({
-      totalRows: paginate.count,
-      collections: paginate.result,
-    });
+    if (paginate.result)
+      return res.json({
+        totalRows: paginate.count,
+        collections: paginate.result.map((c) => ({
+          address: c.address,
+          name: c.name,
+          symbol: c.symbol,
+          type: c.type,
+          createTxHash: c.creationTxHash,
+          createBlockNum: c.firstSeen.number,
+          createTimestamp: c.firstSeen.number,
+          creator: c.master,
+        })),
+      });
   };
 
   /*
@@ -64,8 +74,26 @@ return: nft list [nft Address, nftCreator, nftName, nftSymbol, nftType, nftToken
     const paginate = await this.nftRepo.paginateByAddress(address, page, limit);
     return res.json({
       totoalRows: paginate.count,
-      nfts: paginate.result.map((n) => {}),
-      contract,
+      nfts: paginate.result.map((n) => ({
+        address: n.address,
+        tokenId: n.tokenId,
+        type: n.type,
+        tokenURI: n.tokenURI || '',
+        tokenJSON: n.tokenJSON || '{}',
+
+        mediaURI: n.mediaURI || '',
+        mediaType: n.mediaType || '',
+        minter: n.minter,
+        createTxHash: n.creationTxHash,
+        createBlockNum: n.block.number,
+        createTimestamp: n.block.timestamp,
+      })),
+      collection: {
+        name: contract.name,
+        symbol: contract.symbol,
+        creator: contract.master,
+        type: contract.type,
+      },
     });
   };
 }
